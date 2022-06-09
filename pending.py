26896c39cd37a410b4720f0b001b9f54b659387c
Lua <69074210+26Lua@users.noreply.github.com>
2022-06-09 20:36:29 +0800

    Update pending.py

--- pending.py
+++ pending.py
@@ -7,3 +7,6 @@ Title = input("Thread Reason: ")
 with open ('pendings.txt', 'a') as p:
     p.write("\nTitle: " + User + " - "  + Title + "\nUser: " + User + "\nReason: " + Reason + "\nPunishment: " + Punishment + "\nReference: " + Reference + "\n")
 
+print("\nTitle: " + User + " - "  + Title + "\nUser: " + User + "\nReason: " + Reason + "\nPunishment: " + Punishment + "\nReference: " + Reference + "\n")
+
+input("Press enter to exit ;)")
