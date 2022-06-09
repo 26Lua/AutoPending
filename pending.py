b6ea8a89b8ca3538fb78fb7d842504ddc0b6369d
Lua <69074210+26Lua@users.noreply.github.com>
2022-06-09 20:28:40 +0800

    Update pending.py

--- pending.py
+++ pending.py
@@ -2,8 +2,8 @@ User = input("User: ")
 Reason = input("Reason: ")
 Punishment = input("Punishment: ")
 Reference = input("Reference: ")
+Title = input("Thread Reason: ")
 
 with open ('pendings.txt', 'a') as p:
-    p.write("\nUser: " + User + "\nReason: " + Reason + "\nPunishment: " + Punishment + "\nReference: " + Reference + "\n")
+    p.write("\nTitle: " + User + " - "  + Title + "\nUser: " + User + "\nReason: " + Reason + "\nPunishment: " + Punishment + "\nReference: " + Reference + "\n")
 
-print("\nUser: " + User + "\nReason: " + Reason + "\nPunishment: " + Punishment + "\nReference: " + Reference + "\n\nCopy paste ready! Info has also been saved to pendings.txt")
